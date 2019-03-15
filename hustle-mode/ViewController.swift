871162e93d321895bcb388e00e297127edfe7b91
iPpanther <tang.peerapol@gmail.com>
2019-03-15 11:00:41 +0530

    Initial Commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Peerapol on 15/3/2562 BE.
+//  Copyright © 2562 iPpanther. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
